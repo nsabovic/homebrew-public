1ef7b3958b4ca268c896f158108ece9f4241d8f2
Nebojsa Sabovic <nsabovic@gmail.com>
2018-04-10 11:49:39 -0700

    Fixed a warning.

--- Formula/elf-header.rb
+++ Formula/elf-header.rb
@@ -4,7 +4,7 @@ class ElfHeader < Formula
   url "https://github.com/nsabovic/linuxonmac-elf/archive/1.0.tar.gz"
   sha256 "5717e64d78820fb51c348c0806dc09869518b793c7e12cfa41cbeaf9dd027974"
 
-  depends_on "libelf" => :run
+  depends_on "libelf"
 
   def install
     include.install "elf.h"
